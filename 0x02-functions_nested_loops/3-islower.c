84b3473e554ec54be3433041a4e1a11727dd528a
sayneric <saynerical@gmail.com>
2023-03-22 17:05:02 -0700

    a function that prints the sign of a number.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,10 @@
 /**
  * _islower - check if is lowercase
  * @c: is the char to be checked
+ *_islower: character that are lowercase
+ * return 1 if character is lowercase, return 0 if uppercase
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to be checked
+ *
+ * Return: 1 if n is positve, -1 if n is negative, 0 if n is zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

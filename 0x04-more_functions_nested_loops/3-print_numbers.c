e17cc4183f7066b3aa63a157f9e4fbbfb0d4fca1
sayneric <saynerical@gmail.com>
2023-03-24 21:22:56 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_numbers - print number from 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,12 +3,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the charcter to print 
+ * @c: the charcter to print
  * Return: 1 on success
  * 0n error: -1 is returned and error is set appropriately
- * */
+ */
 
 int _putchar(char c)
 {
-	return (write(1, %c, 1))
+	return (write(1, &c, 1));
 }

760ec444664bbbc3fb5042631cbdcc3d5d335d09
sayneric <saynerical@gmail.com>
2023-03-24 21:58:54 -0700

    clear

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1')

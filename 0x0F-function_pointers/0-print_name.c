64e57174ffa08c4ed21e0c9a4c31eae1afc6a1b3
sayneric <saynerical@gmail.com>
2023-04-19 19:57:25 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
- * Return: nothing 
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))

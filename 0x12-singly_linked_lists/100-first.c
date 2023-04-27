d252e4aecf93eda1e79721388cb415a8c5140185
sayneric <saynerical@gmail.com>
2023-04-27 12:27:45 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void first(void)__attribute__((constructor));
  */
 void first(void)
 {
-	printf("you're beat! and yet, you must allow,\n");
-	printf("i bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

e5c5d189017d4008066662d74d09b075872dbd65
sayneric <saynerical@gmail.com>
2023-04-30 02:53:31 -0700

    update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,16 +11,19 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *current;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
-	while (new)
+	*head = new;
+	current = *head;
+	while (current)
 	{
-		printf("%d\n", new->n);
-		new = new->next;
+		printf("%d\n", current->n);
+		current = current->next;
 	}
 	return (new);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,6 +24,8 @@ size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 
 #endif

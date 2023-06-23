55c2c2553370360a9a151b84ca6e60f87a73466f
sayneric <saynerical@gmail.com>
2023-06-23 11:48:12 -0700

    debugged my 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,8 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, int numb2;
+	int num1;
+	int num2;
 	char *op;
 
 	if (argc != 4)
@@ -22,14 +23,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 	num1 = atoi(argv[1]);
-	op = agrv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0)  ||  (*op == '%' && num2 == 0));
+	if ((*op == '/' && num2 == 0)  ||  (*op == '%' && num1 == 0))
 	{
 		printf("Error\n");
 		exit(100);

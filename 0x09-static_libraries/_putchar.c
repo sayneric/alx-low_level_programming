4720281caad2bc4ee02fe825a6a2ea62b1b4c38f
sayneric <saynerical@gmail.com>
2023-07-09 11:17:09 -0700

    README.md, main.h and _putchar.c file

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,15 +1,13 @@
-#include "main.h"
-#include <stdio.h>
 #include <unistd.h>
 
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the charcter to print
- * Return: 1 on success
- * 0n error: -1 is returned and error is set appropriately
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
